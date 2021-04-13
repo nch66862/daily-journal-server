07482a764d1406c2dbfe671fe3a256dd2d2289d4
Nick Carver <nicholascarver2@outlook.com>
2021-04-13 15:14:57 -0500

    first commit

---
+++ models/entry.py
@@ -0,0 +1,11 @@
+class Entry():
+
+    # Class initializer. It has 5 custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, id, concept, entry, date, moodId):
+        self.id = id
+        self.concept = concept
+        self.entry = entry
+        self.date = date
+        self.moodId = moodId
