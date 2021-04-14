23004ad919c932507c706bf1d5e3cba33fc87223
Nick Carver <nicholascarver2@outlook.com>
2021-04-14 15:54:56 -0500

    added search function

--- entries/request.py
+++ entries/request.py
@@ -70,6 +70,45 @@ def get_single_entry(id):
 
         return json.dumps(entry.__dict__)
 
+def get_entries_by_search(terms):
+    with sqlite3.connect("./dailyjournal.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.concept,
+            e.entry,
+            e.date,
+            e.moodId
+        FROM entry e
+        WHERE e.entry LIKE ?
+        """, ( terms, ))
+
+        # Initialize an empty list to hold all entry representations
+        entries = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an entry instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Entry class above.
+            entry = Entry(row['id'], row['concept'], row['entry'],
+                            row['date'], row['moodId'])
+
+            entries.append(entry.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(entries)
+
 def delete_entry(id):
     with sqlite3.connect("./dailyjournal.db") as conn:
         db_cursor = conn.cursor()
--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from entries import get_all_entries, get_single_entry, delete_entry
+from entries import get_all_entries, get_single_entry, delete_entry, get_entries_by_search
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -9,25 +9,33 @@ from entries import get_all_entries, get_single_entry, delete_entry
 class HandleRequests(BaseHTTPRequestHandler):
 
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
         path_params = path.split("/")
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
-        try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals . pass tells code to keep running if hitting this kind of error
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
 
-        return (resource, id)  # This is a tuple
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /animals
+            except ValueError:
+                pass  # Request had trailing slash: /animals/
+
+            return (resource, id)
 
     # Here's a class function
     def _set_headers(self, status):
@@ -49,20 +57,38 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        # the following code overwrites what the do_GET contains. do_GET is a function in BaseHTTPRequestHandler
         self._set_headers(200)
-        response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned. Unpacks(like object destructuring) the tuple from the parse_url function above
-        (resource, id) = self.parse_url(self.path)
+        response = {}
 
-        if resource == "entries":
-            if id is not None:
-                response = get_single_entry(id)
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
+
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/animals` or `/animals/2`
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
+
+            if resource == "entries":
+                if id is not None:
+                    response = get_single_entry(id)
+                else:
+                    response = get_all_entries()
             else:
-                response = get_all_entries()
-        else:
-            response = "[]"
+                    response = "[]"
+
+        # Response from parse_url() is a tuple with 3
+        # items in it, which means the request was for
+        # `/resource?parameter=value`
+        elif len(parsed) == 3:
+            ( resource, key, value ) = parsed
+
+            # Is the resource `customers` and was there a
+            # query parameter that specified the customer
+            # email as a filtering value?
+            if key == "q" and resource == "entries":
+                response = get_entries_by_search(value)
 
         self.wfile.write(response.encode())
 
